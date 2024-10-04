6801a67d7642d0f650a3291d56f05424b0b4bcfa
omgirimonte <owen.girimonte@gmail.com>
2024-10-04 17:38:09 +0000

    end of 10/4/2024

--- WordSorterManager/WordSorterManager.java
+++ WordSorterManager/WordSorterManager.java
@@ -18,7 +18,8 @@ public class WordSorterManager {
         list[letters.indexOf(word.toLowerCase().charAt(0))].add(word.toLowerCase());
     }
 
-    public void getInput (Scanner input){
+    public void getInput (){
+        Scanner input = new Scanner(System.in);
         boolean run = true;
         while (run){
             System.out.println("Choose an option from below:");
@@ -29,7 +30,7 @@ public class WordSorterManager {
             System.out.println("(5) Remove a word from the data structure  ");
             System.out.println("(6) Exit  ");
             int ans = input.nextInt();
-
+            input.nextLine();
             if (ans==1){
                 System.out.print("Please enter your specefic letter: ");
                 char letter = input.nextLine().charAt(0);
@@ -37,22 +38,57 @@ public class WordSorterManager {
                     System.out.println(word);
             }
             else if(ans==2){
-                
+                for(int x = 0; x<list.length;x++){
+                    for(int i =0; i<list[x].size(); i++){
+                        System.out.println(list[x].get(i));
+                    }
+                }
             }
             else if(ans==3){
-                
+                //for (int x =0; list.length; )
             }
             else if(ans==4){
-                
+                System.out.println("What is the word you want to find? ");
+                String word = input.nextLine();
+                boolean found = false;
+                ArrayList<String> cur = list[letters.indexOf(word.toLowerCase().charAt(0))];
+                for(int x = 0; x<cur.size();x++){
+                        if (cur.get(x).equals(word)){
+                            found=true;
+                            break;
+                        }
+                        if (found){
+                            System.out.println("The word was found in the list!");
+                        }
+                        else{
+                            System.out.println("The word was NOT found in the list!");
+                        }
             }
             else if(ans==5){
                 System.out.println("What is the word you want removed? ");
                 String rem = input.nextLine();
-                //ArrayList<String>[] temporary = ;
+                boolean found = false;
+                ArrayList<String> temporary = list[letters.indexOf(rem.toLowerCase().charAt(0))];
+                for(int x = 0; x<temporary.size();x++){
+                        if (temporary.get(x).equals(rem)){
+                            temporary.remove(x);
+                            x--;
+                            found=true;
+                            //break;
+                        }
+                        //list[letters.indexOf(rem.toLowerCase().charAt(0))] = temporary;
+                        if (found){
+                            System.out.println("The word was found in the list!");
+                        }
+                        else{
+                            System.out.println("The word was NOT found in the list!");
+                        }
+                }
             }
             else if(ans==6){
                 run = false;
             }
         }
+        input.close();
     }
 }
